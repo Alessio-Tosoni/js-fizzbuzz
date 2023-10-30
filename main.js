25b3f67b8e3bc456ed58c4a554f6e4c22eddda94
Alessio-Tosoni <alessiotosoni988@gmail.com>
2023-10-30 16:11:18 +0100

    1 tentativo

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 for (let i = 1; i < 100; i++) {
    
 
-   if(i + 3 ){
+   if(i % 3 == 0 ){
     console.log("fizz")
-   } else if(i + 5){
+   } else if(i % 5 == 0){
     console.log("buzz")
+   }else if (i % 15 == 0){
+    console.log("fizzbuzz")
    }
+
 }
